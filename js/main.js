ab1a8b63cea67dc0a0b1e997c18f2e2001c453b2
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2024-11-25 10:13:22 +0500

    start

---
+++ js/main.js
@@ -0,0 +1,137 @@
+// 1-misol
+// a)
+// Birinchi uch burchak
+// var a = 4
+// var b = 5
+// var c = 7
+// var p = a + b + c
+// console.log("birinchi uch burchak perimetiri", p);
+// Ikkinchi uch burchak
+// var a = 6
+// var b = 8
+// var c = 7
+// var p = a + b + c
+// console.log("ikkinchi uch burchak perimetiri", p);
+// Uchinchi uch burchak
+// var a = 3
+// var b = 4
+// var c = 5
+// var p = a + b + c
+// console.log("ikkinchi uch burchak perimetiri", p);
+// Uchta uch burchak o'rta arifmetigi
+// var a = 16
+// var b = 21
+// var c = 12
+// var javob = (a + b + c) / 3
+// console.log("uchta uch burchak o'ra arifmetigi", javob);
+
+// 2-misol
+// var a = 10
+// var S = 10 * 10
+// console.log("Bog'ning yuzi", S);
+// 3-misol
+// alert("Assalom alaykum")
+// var ism = prompt("ismingiz kiriting");
+// var yosh = +prompt("yoshingizni kiriting");
+// var a = 2024
+// var yil = a - yosh
+// console.log("Assalom alaykum", ism, "siz ", yil, "yilda tug'ilgansiz");
+
+// 4-misol
+// var yil = +prompt("Hozirgi yil")
+// var oy = +prompt("Hozirgi oy")
+// var kun = +prompt("Hozirgi kun")
+// var y = (2050 - yil - 1) * 365
+// var o = (12 - oy) * 30
+// var k = 30 - kun
+// var natija = y + o + k
+// console.log(natija, "kun qolibdi");
+
+// 5-misol
+// var a = +prompt("Sonni kiriting")
+// var b = +prompt("Sonni kiriting")
+// var c = +prompt("Sonni kiriting")
+// var yigindisi = (a + b + c)
+// var kopaytmasi = (a * b * c)
+// var orta = (yigindisi + kopaytmasi) / 2
+// console.log(yigindisi, kopaytmasi, orta);
+
+// 5-misol
+// a)
+// var a = 8
+// var p =8*4
+// var s =8*8
+// console.log("Yuzi=", s, "Perimetiri=", p);
+
+// b)
+// var ism = prompt("Ismingiz kiriting");
+// var yosh = +prompt("Yoshingizni kiriting");
+// var kasb = prompt("Kasbingizni kiriting");
+// var a = 2024
+// var yil = a - yosh
+// console.log("Assalom alaykum", ism, "siz ", yil, "yilda tug'ilgansiz", "Kasbingiz", kasb);
+
+// c)
+var yil = +prompt("Hozirgi yil")
+var oy = +prompt("Hozirgi oy")
+var kun = +prompt("Hozirgi kun")
+var y = (2030 - yil - 1) * 365
+var o = (12 - oy) * 30
+var k = 30 - kun
+var natija = y + o + k
+console.log(natija, "kun qolibdi");
+
+// d)
+// var a = +prompt("Sonni kiriting")
+// var b = +prompt("Sonni kiriting")
+// var c = +prompt("Sonni kiriting")
+// var yigindisi = (a + b + c)
+// var kopaytmasi = (a * b * c)
+// var orta = (yigindisi + kopaytmasi) / 2
+// console.log(yigindisi, kopaytmasi, orta);
+
+// e)
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let c = a + b;
+// let d = a * b;
+// let e = a * a;
+// let f = b * b;
+// console.log("Yig'indisi", c, "Ko'paytmasi", d, "Birinchi sonni kivadirati", e, "Ikkinchi sonni kivadirati", f);
+
+// f)
+// Birinchi uch burchak
+// var a = 4
+// var b = 5
+// var c = 7
+// var p = a + b + c
+// console.log("birinchi uch burchak perimetiri", p);
+// // Ikkinchi uch burchak
+// var a = 6
+// var b = 8
+// var c = 7
+// var p = a + b + c
+// console.log("ikkinchi uch burchak perimetiri", p);
+// // Uchta uch burchak o'rta arifmetigi
+// var a = 16
+// var b = 21
+// // var c = 12
+// var javob = (a + b + c) / 3
+// console.log("uchta uch burchak o'ra arifmetigi", javob);
+
+// g)
+// var a = 50;
+// var ism = prompt("Ismingiz kiriting");
+// var b = +prompt("Yoshingizni kiriting");
+// var c = a - b;
+// console.log(ism, "Siz 50 yoshga kirishingizga", c, "yil bor");
+
+
+
+
+
+
+
+
+
+
